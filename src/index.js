116b800cb510a4d5ae17ea82cd64cf9682085853
marocena26 <mariaelena.arocena@gmail.com>
2022-12-22 14:13:45 +0100

    start with template engines and static server

--- src/index.js
+++ src/index.js
@@ -1,33 +1,42 @@
 // Fichero src/index.js
 
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require('express');
-const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 // Creamos el servidor
 const server = express();
+server.set("view engine", "ejs");
 
 // Configuramos el servidor
-server.use(express.json({ limit: '25mb' }));
+server.use(express.json({ limit: "25mb" }));
 server.use(cors());
 
 //creamos un array para las tarjetas
 const cards = [];
 
 // Crear la tarjeta en el servidor
-server.post('/card', (req, res) => {
-  if (req.body.palette === '' || req.body.name === '' || req.body.job === '' || req.body.photo === '' || req.body.phone === '' || req.body.email === '' || req.body.github === '' || req.body.linkedin === '') {
+server.post("/card", (req, res) => {
+  if (
+    req.body.palette === "" ||
+    req.body.name === "" ||
+    req.body.job === "" ||
+    req.body.photo === "" ||
+    req.body.phone === "" ||
+    req.body.email === "" ||
+    req.body.github === "" ||
+    req.body.linkedin === ""
+  ) {
     const errorMsg = {
       success: false,
-      error: 'faltan datos'
-    }
+      error: "faltan datos",
+    };
     res.json(errorMsg);
-  }
-  else {
+  } else {
     const newCard = { id: uuidv4(), ...req.body };
     cards.push(newCard);
-    console.log(newCard.id)
+    console.log(newCard.id);
     const idCard = newCard.id;
     const response = {
       success: true,
@@ -37,9 +46,13 @@ server.post('/card', (req, res) => {
   }
 });
 
-server.get('/card/:id', (req, res) => {
-  res.json({ message: 'holis' });
-  //express III
+// A REVISAR
+server.get("/card/:id", (req, res) => {
+  console.log(req.params);
+  const foundCards = cards.find(
+    (card) => parseInt(card.id) === parseInt(req.params.id)
+  );
+  res.render("card", foundCards);
 });
 
 // Arrancamos el servidor en el puerto 3000
@@ -49,5 +62,5 @@ server.listen(serverPort, () => {
 });
 
 //servidor estático
-const staticServerPath = './src/public-react';
+const staticServerPath = "./src/public-react";
 server.use(express.static(staticServerPath));
