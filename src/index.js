c9b83e6e9050dba60388df884b97dd9fd144565f
Elena <elenavargasm9@gmail.com>
2022-12-23 13:49:47 +0100

    add data base I

--- src/index.js
+++ src/index.js
@@ -1,58 +1,66 @@
 // Fichero src/index.js
 
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require("express");
-const cors = require("cors");
-const { v4: uuidv4 } = require("uuid");
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
+const Database = require('better-sqlite3');
+
+//base de datos
+const db = new Database('./src/db/database.db', { verbose: console.log });
 
 // Creamos el servidor
 const server = express();
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
 
 // Configuramos el servidor
-server.use(express.json({ limit: "25mb" }));
+server.use(express.json({ limit: '25mb' }));
 server.use(cors());
 
 //creamos un array para las tarjetas
 const cards = [];
 
 // Crear la tarjeta en el servidor
-server.post("/card", (req, res) => {
+server.post('/card', (req, res) => {
   if (
-    req.body.palette === "" ||
-    req.body.name === "" ||
-    req.body.job === "" ||
-    req.body.photo === "" ||
-    req.body.phone === "" ||
-    req.body.email === "" ||
-    req.body.github === "" ||
-    req.body.linkedin === ""
+    req.body.palette === '' ||
+    req.body.name === '' ||
+    req.body.job === '' ||
+    req.body.photo === '' ||
+    req.body.phone === '' ||
+    req.body.email === '' ||
+    req.body.github === '' ||
+    req.body.linkedin === ''
   ) {
     const errorMsg = {
       success: false,
-      error: "faltan datos",
+      error: 'faltan datos',
     };
     res.json(errorMsg);
   } else {
     const newCard = { id: uuidv4(), ...req.body };
     cards.push(newCard);
-    console.log(newCard.id);
+    // console.log(cards);
+    // console.log(newCard.id);
     const idCard = newCard.id;
     const response = {
       success: true,
-      cardUrl: `//localhost:4000/card/:${idCard}`,
+      cardURL: `//localhost:4000/card/${idCard}`,
     };
+    // console.log(response);
     res.json(response);
   }
 });
 
 // A REVISAR
-server.get("/card/:id", (req, res) => {
-  console.log(req.params);
-  const foundCards = cards.find(
-    (card) => parseInt(card.id) === parseInt(req.params.id)
-  );
-  res.render("card", foundCards);
+server.get('/card/:id', (req, res) => {
+  const query = db.prepare('SELECT * FROM cards WHERE id=?');
+  const foundCards = query.get(req.params.id);
+  // console.log(cards);
+  // console.log(req.params);
+  // const foundCards = cards.find((card) => `:${card.id}` === req.params.id);
+  console.log(foundCards);
+  res.render('card', foundCards);
 });
 
 // Arrancamos el servidor en el puerto 3000
@@ -62,5 +70,5 @@ server.listen(serverPort, () => {
 });
 
 //servidor estático
-const staticServerPath = "./src/public-react";
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
